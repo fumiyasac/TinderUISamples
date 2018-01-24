0f03707782b3e4e5ee18ee1f84dafaff5ff8141c
Fumiya Sakai <just1factory@gmail.com>
2018-01-24 10:10:11 +0900

    Initial Commit

---
+++ TinderUISamples/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinderUISamples
+//
+//  Created by 酒井文也 on 2018/01/24.
+//  Copyright © 2018年 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
